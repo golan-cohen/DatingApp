8940c1a6c050aa6d70b71c8fe3c4ff827eca990a
golan-cohen <golanco99@gmail.com>
2021-07-08 13:31:33 +0300

    End of section 4

--- API/Data/Migrations/20210705112637_InitialCreate.cs
+++ API/Data/Migrations/20210705112637_InitialCreate.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace API.Data.Migrationste
+namespace API.Data.Migrations
 {
     public partial class InitialCreate : Migration
     {
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,6 +4,9 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public byte[] passwordHash { get; set; }
+        public byte[] passwordSlat { get; set; }
         
     }
 }
